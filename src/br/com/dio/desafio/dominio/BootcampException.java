491eb39e22dd07743cb1ce5ccc66703025382481
jggrimaldi <jggrimaldi12@gmail.com>
2026-02-24 11:46:27 -0300

    refactor: aplica streams e corrige estrutura de pacotes na classe Dev

---
+++ src/br/com/dio/desafio/dominio/BootcampException.java
@@ -0,0 +1,7 @@
+package br.com.dio.desafio.dominio;
+
+public class BootcampException extends RuntimeException {
+    public BootcampException(String message) {
+        super(message);
+    }
+}
--- src/br/com/dio/desafio/dominio/Dev.java
+++ src/br/com/dio/desafio/dominio/Dev.java
@@ -3,40 +3,40 @@ package br.com.dio.desafio.dominio;
 import java.util.*;
 
 public class Dev {
+
     private String nome;
-    private Set<Conteudo> conteudosInscritos = new LinkedHashSet<>();
-    private Set<Conteudo> conteudosConcluidos = new LinkedHashSet<>();
+
+    private final Set<Conteudo> conteudosInscritos = new LinkedHashSet<>();
+    private final Set<Conteudo> conteudosConcluidos = new LinkedHashSet<>();
 
     public void inscreverBootcamp(Bootcamp bootcamp){
+        // Validação defensiva
+        if (bootcamp == null) {
+            throw new IllegalArgumentException("O bootcamp não pode ser nulo.");
+        }
         this.conteudosInscritos.addAll(bootcamp.getConteudos());
         bootcamp.getDevsInscritos().add(this);
     }
 
     public void progredir() {
-        Optional<Conteudo> conteudo = this.conteudosInscritos.stream().findFirst();
-        if(conteudo.isPresent()) {
-            this.conteudosConcluidos.add(conteudo.get());
-            this.conteudosInscritos.remove(conteudo.get());
-        } else {
-            System.err.println("Você não está matriculado em nenhum conteúdo!");
-        }
+        // MELHORIA: Uso de ifPresentOrElse ou lançamento de exceção
+        this.conteudosInscritos.stream()
+                .findFirst()
+                .ifPresentOrElse(conteudo -> {
+                    this.conteudosConcluidos.add(conteudo);
+                    this.conteudosInscritos.remove(conteudo);
+                }, () -> {
+                    // Em vez de System.err, lançamos uma exceção de negócio
+                    throw new NoSuchElementException("Você não está matriculado em nenhum conteúdo!");
+                });
     }
 
     public double calcularTotalXp() {
-        Iterator<Conteudo> iterator = this.conteudosConcluidos.iterator();
-        double soma = 0;
-        while(iterator.hasNext()){
-            double next = iterator.next().calcularXp();
-            soma += next;
-        }
-        return soma;
-
-        /*return this.conteudosConcluidos
+        return this.conteudosConcluidos
                 .stream()
                 .mapToDouble(Conteudo::calcularXp)
-                .sum();*/
+                .sum();
     }
-
 
     public String getNome() {
         return nome;
@@ -47,31 +47,23 @@ public class Dev {
     }
 
     public Set<Conteudo> getConteudosInscritos() {
-        return conteudosInscritos;
-    }
-
-    public void setConteudosInscritos(Set<Conteudo> conteudosInscritos) {
-        this.conteudosInscritos = conteudosInscritos;
+        return Collections.unmodifiableSet(conteudosInscritos);
     }
 
     public Set<Conteudo> getConteudosConcluidos() {
-        return conteudosConcluidos;
-    }
-
-    public void setConteudosConcluidos(Set<Conteudo> conteudosConcluidos) {
-        this.conteudosConcluidos = conteudosConcluidos;
+        return Collections.unmodifiableSet(conteudosConcluidos);
     }
 
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+        if (!(o instanceof Dev)) return false;
         Dev dev = (Dev) o;
-        return Objects.equals(nome, dev.nome) && Objects.equals(conteudosInscritos, dev.conteudosInscritos) && Objects.equals(conteudosConcluidos, dev.conteudosConcluidos);
+        return Objects.equals(nome, dev.nome);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(nome, conteudosInscritos, conteudosConcluidos);
+        return Objects.hash(nome);
     }
 }
